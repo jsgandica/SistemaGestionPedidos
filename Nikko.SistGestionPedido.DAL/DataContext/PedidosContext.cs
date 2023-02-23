ec2269318d4183c83b170cf1012121ee06de9071
Julio Morfe <julioskay1@gmail.com.com>
2023-02-23 09:24:07 -0400

    Cambio en Controlador Cliente

--- Nikko.SistGestionPedido.DAL/DataContext/PedidosContext.cs
+++ Nikko.SistGestionPedido.DAL/DataContext/PedidosContext.cs
@@ -35,8 +35,9 @@ public partial class PedidosContext : DbContext
     public virtual DbSet<Vendedor> Vendedors { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=BLACK-LAP-8\\SQLEXPRESS;Initial Catalog=pedidos;Integrated Security=True;TrustServerCertificate=True");
+    { 
+    
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
