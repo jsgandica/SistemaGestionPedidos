713be93c9df2be3175656f8696b066ef6cbb7868
Julio Morfe <julioskay1@gmail.com.com>
2023-02-24 10:39:23 -0400

    Tercer commit hice cambio en Cliente:ControllerCliente (agrege Details), Delete.cshtlm, Index.htlm,  y  Vista de Details. Se comprobo el CRUD

---
+++ Nikko.SistGestionPedido.Models/RolPermiso.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Nikko.SistGestionPedido.Models;
+
+public partial class RolPermiso
+{
+    public int Id { get; set; }
+
+    public string Accion { get; set; } = null!;
+
+    public int RolId { get; set; }
+
+    public bool Activo { get; set; }
+
+    public DateTime Fecha { get; set; }
+}
---
+++ Nikko.SistGestionPedido.UI/Models/ViewModels/ClienteViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Nikko.SistGestionPedido.UI.Models.ViewModels
+{
+    public class ClienteViewModel
+    {
+        public int Id { get; set; }
+
+        public string Nombre { get; set; } = null!;
+
+       
+        public string? Telefono { get; set; }
+
+        
+        public DateTime Fecha { get; set; }
+
+    }
+}
