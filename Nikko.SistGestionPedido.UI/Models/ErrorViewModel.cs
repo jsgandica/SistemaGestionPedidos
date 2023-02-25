a38a0ecb723187668fac301801b3a82781284559
Juan Simon Gandica Cardel <gandicajuan@outlook.com>
2023-02-25 00:15:10 -0400

    Revesrso

--- Nikko.SistGestionPedido.UI/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Nikko.SistGestionPedido.UI.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
