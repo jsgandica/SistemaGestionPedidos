057157cc747be1ed918196e3df246d71561b8a5d
Juan Simon Gandica Cardel <gandicajuan@outlook.com>
2023-02-27 23:47:40 -0400

    Feat: Ajustes en; Todas las vistas de Vendedor. Vista Index de Cliente. Inclusion del DataAnotacion en el ViewModel de Vendedor.

--- Nikko.SistGestionPedido.UI/Models/ViewModels/VendedorViewModel.cs
+++ Nikko.SistGestionPedido.UI/Models/ViewModels/VendedorViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace Nikko.SistGestionPedido.UI.Models.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Nikko.SistGestionPedido.UI.Models.ViewModels
 {
     public class VendedorViewModel
     {
         public int Id { get; set; }
-
+        [Required(ErrorMessage = "El Campo {0} debe ser requerido")]
         public string Nombre { get; set; } = null!;
         public Guid? Rowguid { get; set; }
 
